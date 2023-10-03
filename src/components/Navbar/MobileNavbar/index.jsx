4e354247be2095a7b0c0dc7f99a7cd12c94fdf99
lawrenceuchenye <lawuche29@gmail.com>
2023-10-03 23:13:07 +0100

    minor css bug fix

---
+++ src/components/Navbar/MobileNavbar/index.jsx
@@ -0,0 +1,19 @@
+import "./index.css";
+
+const index = () => {
+  return (
+    <div>
+      <div>
+        <i className="fa fa-home"></i>
+      </div>
+      <div>
+        <i className="fa fa-home"></i>
+      </div>
+      <div>
+        <i className="fa fa-home"></i>
+      </div>
+    </div>
+  );
+};
+
+export default index;
